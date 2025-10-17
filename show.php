d60959555306d303bc78a89b82508b97320a8fef
hmathias02 <helena.mathias20@gmail.com>
2025-10-17 15:57:29 -0300

    commit inicial

--- show.php
+++ show.php
@@ -4,7 +4,7 @@
   <div class="container" id="view-contact-container"> 
     <?php include_once("templates/backbtn.html"); ?>
     <h1 id="main-title"><?= $contact["name"] ?></h1>
-    <p class="bold">Telefone:</p>
+    <p class="bold">Endereço:</p>
     <p class="form-control"><?= $contact["phone"] ?></p>
     <p class="bold">Observações:</p>
     <textarea type="text" class="form-control" id="observations" name="observations" rows="3"><?= $contact['observations'] ?></textarea>
